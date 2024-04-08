8ee17f07525355ee09a432158b82db8f6a4c2404
ornellaCipollone <ocipollone@udesa.edu.ar>
2024-04-08 16:12:32 -0300

    Avanzando en estructura de React

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
-import {Link, Route, Switch} from 'react-router-dom'
+import React from "react";
+import {Link, Route, Switch} from 'react-router-dom';
+
+// SCREENS
+import Home from "./screens/Home/Home";
+
+//COMPONENTES
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
+import Favoritos from "./components/Favoritos/Favoritos";
+import Detail from "./components/Detail/Detail";
+import Resultados from "./components/Resultados/Resultados";
+import Loader from "./components/Loader/Loader";
+import Error404 from "./components/Error404/Error404";
+
 
 function App() {
   return (
