db6358958482a146364db4a5d7be31813475c9d5
ornellaCipollone <ocipollone@udesa.edu.ar>
2024-04-08 16:01:29 -0300

    armando la estructura de carpetas

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import {Link, Route, Switch} from 'react-router-dom'
+
 function App() {
   return (
    <div>
-    
+    <Link to="/">Home</Link>
+
+    <Switch>
+      <Route path="/" component={Home} />
+    </Switch>
+
    </div>
   );
 }
