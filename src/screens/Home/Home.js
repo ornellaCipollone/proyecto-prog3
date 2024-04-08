6e62f2796041ca11741f4b12848a9adea188c16b
LaraManfredini1 <lmanfredini@udesa.edu.ar>
2024-04-08 17:04:23 -0300

    empezando home

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react'
 
-export default class Home extends Component {
+
+
+class Home extends Component {
+  constructor() {
+    super()
+    this.state = {
+      populars: [],
+      topRated: [],
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://api.themoviedb.org/3/movie/popular?api_key=b0c989c37b55d3ea4a70eda0aeea1b02")
+      .then(res => res.json())
+      .then(data => this.setState({
+        populars: data.results,
+      }))
+      .catch(error => console.log(`El error fue ${error}`))
+
+    fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=b0c989c37b55d3ea4a70eda0aeea1b02")
+      .then(res => res.json())
+      .then(data => this.setState({
+        topRated: data.results,
+      }))
+      .catch(error => console.log(`El error fue ${error}`))
+  }
+
+
   render() {
     return (
-      <div>Home</div>
+      <div>
+
+      </div>
     )
   }
 }
-
-
-
+export default Home
 
 
