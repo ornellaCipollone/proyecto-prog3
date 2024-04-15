c758a8e65943167020b48b5ea0f5cf8dfc5263b9
ornellaCipollone <ocipollone@udesa.edu.ar>
2024-04-15 14:53:49 -0300

    avances

--- src/components/Pelicula/Pelicula.js
+++ src/components/Pelicula/Pelicula.js
@@ -4,14 +4,7 @@ import { Link } from "react-router-dom";
 class Pelicula extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-        show: false,
-        descripcion: props.datosPeli.overview
-    };
-    handleshow(){
-        this.setState({
-            show:!this.state.show
-        })
+    this.state = {};
   }
 
   render() {
@@ -24,16 +17,8 @@ class Pelicula extends Component {
         <div>
           <p>{this.props.datosPelicula.title}</p>
           <p>{this.props.datosPelicula.release_date} </p>
-          <p>{this.state.show ? this.state.descripcion : ""} </p>
-          <p onClick={() => this.handleshow()}>
-            {this.state.show ? "Ver menos" : "Ver más"}
-          </p>
-          <Link
-            to={`/detalle/id/${this.props.datosPelicula.id}`}
-            className="detalle"
-          >
-            {" "}
-            Ir a Detalle{" "}
+          <Link to={`/detail/id/${this.props.datosPelicula.id}`}>
+            Ir a Detalle
           </Link>
         </div>
       </>
