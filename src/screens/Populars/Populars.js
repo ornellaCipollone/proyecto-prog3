1bd9ac7589954019985f6ff913a0e278feec609a
LaraManfredini1 <lmanfredini@udesa.edu.ar>
2024-04-15 17:50:05 -0300

    avanzando

--- src/screens/Populars/Populars.js
+++ src/screens/Populars/Populars.js
@@ -1,20 +1,71 @@
 import React, { Component } from "react";
+import Filtro from "../../components/Filtro/Filtro";
 
-class populars extends Component {
-    constructor() {
-        super()
-        this.state = {
-            populars: [],
-        }
-    }
-    componentDidMount() {
-        fetch("https://api.themoviedb.org/3/movie/popular?api_key=b0c989c37b55d3ea4a70eda0aeea1b02")
+class Populars extends Component {
+  constructor() {
+    super();
+    this.state = {
+      populars: [],
+      page: 1,
+    };
+  }
 
-            .then(res => res.json())
-            .then(data => this.setState({
-                populars: data.results
+  componentDidMount() {
+    fetch(
+      `https://api.themoviedb.org/3/movie/top_rated?api_key=b0c989c37b55d3ea4a70eda0aeea1b02&page=${this.state.page}`
+    )
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({
+          populars: data.results,
+          page: this.state.page + 1,
+        })
+      )
+      .catch((error) => console.log(`El error fue ${error}`));
+  }
 
-            }))
-            .catch(error => console.log(error))
-    }
+  filtrarPopulars(filtro) {
+    let peliculasFiltradas = this.state.populars.filter((pelicula) =>
+      pelicula.title.includes(filtro)
+    );
+    this.setState({
+      populars: peliculasFiltradas,
+    });
+  }
+
+  traerMas() {
+    fetch(
+      `https://api.themoviedb.org/3/movie/popular?api_key=b0c989c37b55d3ea4a70eda0aeea1b02&page=${this.state.page}`
+    )
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({
+          populars: this.state.populars.concat(data.results),
+          page: this.state.page + 1,
+        })
+      )
+      .catch((error) => console.log(`El error fue ${error}`));
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <Filtro
+          filtrarPelis={(valorInput) => this.filtrarPopulars(valorInput)}
+        />
+        {this.state.populars.length > 0 ? (
+          this.state.populars.map((pelicula, idx) => (
+            <Pelicula key={pelicula + idx} datosPelicula={pelicula} />
+          ))
+        ) : (
+          <h3> Loading...</h3>
+        )}
+        <button onClick={() => this.traerMas(this.state.populars)}>
+          Ver más
+        </button>
+      </React.Fragment>
+    );
+  }
 }
+
+export default Populars;
