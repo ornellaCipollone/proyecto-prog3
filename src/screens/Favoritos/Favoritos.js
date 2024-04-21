72de8c85e9682899c21854af7527abbaafa51ad7
ornellaCipollone <ocipollone@udesa.edu.ar>
2024-04-21 16:23:04 -0300

    correciones

--- src/screens/Favoritos/Favoritos.js
+++ src/screens/Favoritos/Favoritos.js
@@ -1,57 +1,70 @@
-import React, {Component} from "react"
+import React, { Component } from "react";
 import Pelicula from "../../components/Pelicula/Pelicula";
-import "./favoritos.css"
+import Loader from "../../components/Loader/Loader"
+import "./favoritos.css";
 
-class Favoritos extends Component{
-     constructor(props){
-        super(props);
-        this.state = {
-            favoritas: []
-        }
-        }
-        componentDidMount(){
-            let favs = [];
-            let recuperoStoragePeliculas = localStorage.getItem("favoritos")
-            if (recuperoStoragePeliculas !== null){
-                let pelisArray= JSON.parse(recuperoStoragePeliculas)
-                favs= pelisArray 
-                let  peliculasLista = []
 
-            favs.forEach((id) => {
-                fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=b0c989c37b55d3ea4a70eda0aeea1b02`)
-                .then(response => response.json())
-                .then(favs => { peliculasLista.push(favs);
-                    this.setState({favoritas:peliculasLista})
-                })
-            })
+class Favoritos extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      favoritas: [],
+    };
+  }
+  componentDidMount() {
+    let favs = [];
+    let recuperoStoragePeliculas = localStorage.getItem("favoritos");
+    if (recuperoStoragePeliculas !== null) {
+      let pelisArray = JSON.parse(recuperoStoragePeliculas);
+      favs = pelisArray;
+      let peliculasLista = [];
 
-            }
-        }
-
-        render(){
-            console.log(this.state);
-            return(
-                <React.Fragment>
-                {
-                    this.state.favoritas.length > 0 ?
-                <section className="contenedor">
-                    <button role="boton" id="flecha-derecha" className="flecha-izquierda"><i className="icon-angle-left"></i></button>
-                    <article className="contenedor-peliculas">
-                    <h2 className="titulo tituloFavs">Selección de películas favoritas</h2>
-                        <div className="galeria">{
-                            this.state.favoritas.map(
-                                (peliFav, idx) => <Pelicula key={peliFav + idx} datosPelicula={peliFav}/>
-                            )
-                        }
-                        </div>
-                    </article>
-                    <button role="boton" id="flecha-derecha" className="flecha-derecha"><i className="icon-angle-right"></i></button>
-                </section>: 
-                <h3 className="loading">Loading...</h3>}
-        
-                </React.Fragment>
-            )
+      favs.forEach((id) => {
+        fetch(
+          `https://api.themoviedb.org/3/movie/${id}?api_key=b0c989c37b55d3ea4a70eda0aeea1b02`
+        )
+          .then((response) => response.json())
+          .then((favs) => {
+            peliculasLista.push(favs);
+            this.setState({ favoritas: peliculasLista });
+          });
+      });
     }
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <React.Fragment>
+        {this.state.favoritas.length > 0 ? (
+          <section className="contenedor">
+            <button
+              role="boton"
+              id="flecha-izquierda"
+              className="flecha-izquierda"
+            >
+              <i className="icon-angle-left"></i>
+            </button>
+            <article className="contenedor-peliculas">
+              <h2 className="titulo tituloFavs">
+                Selección de películas favoritas
+              </h2>
+              <div className="galeria">
+                {this.state.favoritas.map((peliFav, idx) => (
+                  <Pelicula key={peliFav + idx} datosPelicula={peliFav} />
+                ))}
+              </div>
+            </article>
+            <button role="boton" id="flecha-derecha" className="flecha-derecha">
+              <i className="icon-angle-right"></i>
+            </button>
+          </section>
+        ) : (
+          <Loader />
+        )}
+      </React.Fragment>
+    );
+  }
 }
-       
-    export default Favoritos
+
+export default Favoritos;
